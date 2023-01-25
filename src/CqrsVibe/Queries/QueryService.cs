ab3f38fcf52041640d2a31021ef7b7915ed42cd3
Vsevolod Elunin <seva.elunin@gmail.com>
2023-01-25 15:59:17 +0300

    fix invoker's caches

--- src/CqrsVibe/Queries/QueryService.cs
+++ src/CqrsVibe/Queries/QueryService.cs
@@ -16,7 +16,7 @@ namespace CqrsVibe.Queries
     public class QueryService : IQueryService
     {
         private readonly IPipe<IQueryHandlingContext> _queryPipe;
-        
+
         private readonly ConcurrentDictionary<Type, Type> _queryHandlerTypesCache =
             new ConcurrentDictionary<Type, Type>();
 
@@ -34,13 +34,13 @@ namespace CqrsVibe.Queries
             {
                 throw new ArgumentNullException(nameof(resolverAccessor));
             }
-            
+
             _queryPipe = Pipe.New<IQueryHandlingContext>(pipeConfigurator =>
             {
                 pipeConfigurator.UseDependencyResolver(resolverAccessor);
-                
+
                 configurePipeline?.Invoke(pipeConfigurator);
-                
+
                 pipeConfigurator.UseHandleQuery(resolverAccessor);
             });
         }
@@ -52,7 +52,8 @@ namespace CqrsVibe.Queries
         /// <param name="cancellationToken"></param>
         /// <typeparam name="TResult">Query result type</typeparam>
         /// <returns>Query result</returns>
-        public async Task<TResult> QueryAsync<TResult>(IQuery<TResult> query, CancellationToken cancellationToken = default)
+        public async Task<TResult> QueryAsync<TResult>(IQuery<TResult> query,
+            CancellationToken cancellationToken = default)
         {
             if (query == null)
             {
@@ -60,18 +61,16 @@ namespace CqrsVibe.Queries
             }
 
             var queryType = query.GetType();
-            
-            if (!_queryHandlerTypesCache.TryGetValue(queryType, out var queryHandlerType))
-            {
-                queryHandlerType = typeof(IQueryHandler<,>).MakeGenericType(queryType, typeof(TResult));
-                _queryHandlerTypesCache.TryAdd(queryType, queryHandlerType);
-            }
+
+            var queryHandlerType = _queryHandlerTypesCache.GetOrAdd(
+                queryType,
+                queryTypeArg => typeof(IQueryHandler<,>).MakeGenericType(queryTypeArg, typeof(TResult)));
 
             var contextConstructor = QueryContextCtorFactory.GetOrCreate(queryType, typeof(TResult));
             var context = contextConstructor.Construct(query, queryHandlerType, cancellationToken);
 
             await _queryPipe.Send(context);
-            return ((Task<TResult>) context.ResultTask).Result;
+            return ((Task<TResult>)context.ResultTask).Result;
         }
 
         /// <inheritdoc />
@@ -93,13 +92,9 @@ namespace CqrsVibe.Queries
 
             public static QueryContextConstructor GetOrCreate(Type queryType, Type resultType)
             {
-                if (!ContextConstructorsCache.TryGetValue(queryType, out var contextConstructor))
-                {
-                    contextConstructor = QueryContextConstructor.Compile(queryType, resultType);
-                    ContextConstructorsCache.TryAdd(queryType, contextConstructor);
-                }
-
-                return contextConstructor;
+                return ContextConstructorsCache.GetOrAdd(queryType,
+                    (queryTypeArg, resultTypeArg) => QueryContextConstructor.Compile(queryTypeArg, resultTypeArg),
+                    resultType);
             }
         }
 
@@ -121,7 +116,7 @@ namespace CqrsVibe.Queries
             public Type ContextType { get; }
 
             public IQueryHandlingContext Construct(
-                IQuery query, 
+                IQuery query,
                 Type handlerType,
                 CancellationToken cancellationToken)
             {
@@ -136,24 +131,25 @@ namespace CqrsVibe.Queries
             }
 
             private static Func<IQuery, Type, CancellationToken, IQueryHandlingContext> CompileCtorInvoker(
-                Type queryType, 
+                Type queryType,
                 Type contextType)
             {
                 var contextConstructorInfo = contextType.GetConstructor(
                     BindingFlags.Public | BindingFlags.Instance,
                     null,
-                    new[] {queryType, typeof(Type), typeof(CancellationToken)},
+                    new[] { queryType, typeof(Type), typeof(CancellationToken) },
                     null);
-                
+
                 var queryParameter = Expression.Parameter(typeof(IQuery), "query");
                 var handlerInterfaceParameter = Expression.Parameter(typeof(Type), "handlerInterface");
                 var cancellationTokenParameter = Expression.Parameter(typeof(CancellationToken), "cancellationToken");
 
                 var concreteQueryInstance = Expression.Variable(queryType, "concreteQuery");
 
-                var block = Expression.Block(new[] {concreteQueryInstance},
+                var block = Expression.Block(new[] { concreteQueryInstance },
                     Expression.Assign(concreteQueryInstance, Expression.Convert(queryParameter, queryType)),
-                    Expression.New(contextConstructorInfo!, concreteQueryInstance, handlerInterfaceParameter, cancellationTokenParameter));
+                    Expression.New(contextConstructorInfo!, concreteQueryInstance, handlerInterfaceParameter,
+                        cancellationTokenParameter));
 
                 var constructorInvoker =
                     Expression.Lambda<Func<IQuery, Type, CancellationToken, QueryHandlingContext>>(
